0a4067b1591fb9047f66d1bed45934239d9aa4b7
Juan Diego <desarrollo1@coltek.com.co>
2025-04-21 03:47:30 -0500

    change url real

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     },
   },
   plugins: [vue()],
-  base: 'https://JuanDriko/github.com/FavoritesPokemons/'
+  base: 'https://juandriko.github.io/FavoritesPokemons/',
 })
